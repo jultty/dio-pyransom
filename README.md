d37ecf43a44fd99021fca1492a2e71b64d712d7f
jultty <juno.t@tuta.io>
2023-05-18 16:36:49 -0300

    Improve test readability

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 Desafio de projeto **"Criando um Ransomware com Python"**, desenvolvido para a formação _Cybersecurity Specialist_ da DIO.
 
-Para rodar os testes, use `./dev gui` ou `./dev cli`. O script monitora as mudanças no diretório, e exibe as imagens ou faz verificações usando um hash em sha256.
+Para rodar os testes, use `./dev gui` ou `./dev cli`. O script monitora as mudanças no diretório, e exibe as imagens ou faz verificações usando um hash sha256.
 
 O script de testes requer os pacotes `feh` e `inotify-tools.`
 
-Melhorias realizadas:
+**Melhorias realizadas**:
 
 - Trabalha de forma recursiva passando por todos os subdiretórios e arquivos
 - Inclui um script para testes automatizados durante o desenvolvimento
--- dev.sh
+++ dev.sh
@@ -7,29 +7,45 @@ if [[ "$1" != "cli" && "$1" != "gui" ]]; then
   echo 'Usage: ./dev.sh <gui|cli>'
 else
 
+  function info_text {
+      echo -e "\n ${fmt_yellow}$1${fmt_clear}"
+  }
+  
+  function pass {
+    echo -e "\n ${fmt_green}SUCCESS${fmt_clear}\n"
+  }
+
+  function fail {
+    echo -e "\n ${fmt_red}FAIL${fmt_clear}\n"
+  }
+
   function reset_test_root {
 
-    echo -e "\n${fmt_yellow}Preparing environment${fmt_clear}\n"
+    info_text "Preparing environment"
 
     if [[ ! -f 'test_root.zip' ]]; then
       mv test_root/test_root.zip .
     fi
 
-    rm test_root/*.jpg
+    rm -f test_root/*.jpg
+    rm -f test_root/*.aes
+    rm -f test_root/erebia_aethiops
     unzip -qo test_root.zip -d test_root
   }
 
+  clear
   if [[ "$1" == 'gui' ]]; then
 
     while true; do
       reset_test_root
+      info_text "Displaying pre-test state" &&
       feh test_root/erebia_aethiops.jpg &&
-      python encrypt.py &&
-      file test_root/erebia_aethiops.jpg.aes &&
+      info_text "Testing encryption" &&
+      python encrypt.py && pass &&
+      info_text "Testing decryption" &&
       python decrypt.py &&
-      feh test_root/erebia_aethiops.jpg &&
-        echo -e "\n ${fmt_green}SUCCESS${fmt_clear}\n" ||
-        echo -e "\n ${fmt_red}FAIL${fmt_clear}\n"
+      info_text "Displaying results" &&
+      feh test_root/erebia_aethiops.jpg && pass || fail
       inotifywait -e modify .
       clear
      done
@@ -38,13 +54,16 @@ else
 
     while true; do
       reset_test_root
+      info_text "Getting pre-test state" &&
       sha256sum test_root/* > sha256sums &&
-      python encrypt.py &&
-      file test_root/erebia_aethiops.jpg.aes &&
+      info_text "Testing encryption" &&
+      python encrypt.py && pass &&
+      info_text "Testing decryption" &&
       python decrypt.py &&
-      echo -e "\n$(sha256sum -c sha256sums)" &&
-        echo -e "\n ${fmt_green}SUCCESS${fmt_clear}\n" ||
-        echo -e "\n ${fmt_red}FAIL${fmt_clear}\n"
+      info_text "Displaying results" &&
+      sha256sum -c sha256sums && pass || fail
+      sleep 1
+      echo
       inotifywait -e modify .
       clear
      done
