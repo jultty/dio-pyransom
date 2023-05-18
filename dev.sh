7a97c2382a892a16e45c4938d0799245c4de9f86
jultty <juno.t@tuta.io>
2023-05-18 14:29:07 -0300

    Add formatting for testing script

--- dev.sh
+++ dev.sh
@@ -1,13 +1,20 @@
+fmt_green="\e[32m"
+fmt_red="\e[31m"
+fmt_yellow="\e[33m"
+fmt_clear="\e[0m"
 
 if [[ "$1" != "cli" && "$1" != "gui" ]]; then
   echo 'Usage: ./dev.sh <gui|cli>'
 else
 
-  if [[ ! -f 'test_root.zip' ]]; then
-    mv test_root/test_root.zip .
-  fi
-
   function reset_test_root {
+
+    echo -e "\n${fmt_yellow}Preparing environment${fmt_clear}\n"
+
+    if [[ ! -f 'test_root.zip' ]]; then
+      mv test_root/test_root.zip .
+    fi
+
     rm test_root/*.jpg
     unzip -qo test_root.zip -d test_root
   }
@@ -16,11 +23,13 @@ else
 
     while true; do
       reset_test_root
-      feh test_root/erebia_aethiops.jpg
-      python encrypt.py
-      file test_root/erebia_aethiops.jpg.aes
-      python decrypt.py
-      feh test_root/erebia_aethiops.jpg
+      feh test_root/erebia_aethiops.jpg &&
+      python encrypt.py &&
+      file test_root/erebia_aethiops.jpg.aes &&
+      python decrypt.py &&
+      feh test_root/erebia_aethiops.jpg &&
+        echo -e "\n ${fmt_green}SUCCESS${fmt_clear}\n" ||
+        echo -e "\n ${fmt_red}FAIL${fmt_clear}\n"
       inotifywait -e modify .
       clear
      done
@@ -29,11 +38,13 @@ else
 
     while true; do
       reset_test_root
-      sha256sum test_root/* > sha256sums
-      python encrypt.py
-      file test_root/erebia_aethiops.jpg.aes
-      python decrypt.py
-      echo -e "\n$(sha256sum -c sha256sums) \n"
+      sha256sum test_root/* > sha256sums &&
+      python encrypt.py &&
+      file test_root/erebia_aethiops.jpg.aes &&
+      python decrypt.py &&
+      echo -e "\n$(sha256sum -c sha256sums)" &&
+        echo -e "\n ${fmt_green}SUCCESS${fmt_clear}\n" ||
+        echo -e "\n ${fmt_red}FAIL${fmt_clear}\n"
       inotifywait -e modify .
       clear
      done
